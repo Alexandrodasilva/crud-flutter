252425060bcfc3eafc28447847d8f88ed424afcf
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2023-03-18 16:45:47 -0300

    first commit new project

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: unnecessary_const
+
+import '../models/user.dart';
+
+// ignore: constant_identifier_names
+const DUMMY_USERS = {
+  // ignore: unnecessary_const
+  '1': const User(
+    id: '1',
+    name: 'maria',
+    email: 'maria@aluno.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'fernada',
+    email: 'fernada@aluno.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2023/03/09/13/07/girl-7840010_960_720.jpg'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'morna',
+    email: 'morna@aluno.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
+  ),
+};
